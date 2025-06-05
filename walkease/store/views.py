82d504ee12c49f7a03a0f5a17eb5981408d89953
Farah-94 <farahshigri94@gmail.com>
2025-06-06 00:58:55 +0100

    update index view to render index.html instead of returning a welcome message

--- walkease/store/views.py
+++ walkease/store/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the Store!")
+     return render(request, 'store/index.html')
+
+
 
