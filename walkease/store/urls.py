469e8fbdce65f90bdd17b0fd4a3f3b3d8cedca16
Farah-94 <farahshigri94@gmail.com>
2025-06-06 02:26:01 +0100

    file

--- walkease/store/urls.py
+++ walkease/store/urls.py
@@ -4,4 +4,6 @@ from . import views  # Assumes you have a views.py in the store app
 urlpatterns = [
     # The empty string '' means this view will handle the base URL for this app
     path('', views.index, name='store-index'),
+    path('products/', views.product_list, name='product_list'),
+    path("buy/<int:product_id>/", views.buy_product, name="buy_product"),
 ]
