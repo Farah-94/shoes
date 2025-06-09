52f06bc82550bc805b223183ed8d2755f8e07ffd
Farah-94 <farahshigri94@gmail.com>
2025-06-09 22:48:06 +0100

    index

--- walkease/store/static/store/js/script.js
+++ walkease/store/static/store/js/script.js
@@ -9,3 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
     slides[currentSlide].classList.add("active");
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+      const menuButton = document.getElementById("menu-button");
+      const menuDropdown = document.getElementById("menu-dropdown");
+
+      menuButton.addEventListener("click", function(event) {
+        event.stopPropagation(); // Prevent the click from propagating to the window
+        menuDropdown.classList.toggle("show");
+      });
+
+      // Optional: Hide dropdown when clicking anywhere outside the menu
+      window.addEventListener("click", function() {
+        if (menuDropdown.classList.contains("show")) {
+          menuDropdown.classList.remove("show");
+        }
+      });
+    });
