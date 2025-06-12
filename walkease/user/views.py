56e8f4e8ca91d250d4b7f598d140fc1cd6375b4e
Farah-94 <farahshigri94@gmail.com>
2025-06-13 00:07:26 +0100

    cart

--- walkease/user/views.py
+++ walkease/user/views.py
@@ -19,13 +19,18 @@ def profile_detail(request):
     # Render the profile page
     return render(request, "user/profile.html")
 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import ProfileUpdateForm
+from .models import Profile
+
 @login_required
 def update_profile(request):
     try:
         profile = request.user.profile
     except Profile.DoesNotExist:
         profile = Profile.objects.create(user=request.user)
-
+    
     if request.method == "POST":
         form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
@@ -33,4 +38,4 @@ def update_profile(request):
             return redirect('user:profile_detail')
     else:
         form = ProfileUpdateForm(instance=profile)
-    return render(request, "user/profile.html", {"form": form})
+    return render(request, "user/update_profile.html", {"form": form})
