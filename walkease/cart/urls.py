49c27d501ddf462ebf922909ee39e244e1a37f7d
Farah-94 <farahshigri94@gmail.com>
2025-06-12 22:42:22 +0100

    cart

--- walkease/cart/urls.py
+++ walkease/cart/urls.py
@@ -8,6 +8,6 @@ app_name = "cart"
 urlpatterns = [  
    path("view/", views.cart_view, name="view_cart"), 
    path("signup/", signup, name="signup"),
-   path("login/", auth_views.LoginView.as_view(), name="login"),
+   path("signin/", views.signin, name="signin"),  
    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
