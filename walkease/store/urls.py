6d6f5d1b910b97f0d20b4e9a2d8f6644a5a113c1
Farah-94 <farahshigri94@gmail.com>
2025-06-14 17:37:38 +0100

    store

--- walkease/store/urls.py
+++ walkease/store/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path("products/<slug:slug>/", views.product_list, name="product_list_by_cat"),
     path("products/<int:product_id>/", views.product_detail, name="product_detail"),
     path("products/<int:product_id>/buy/", views.buy_product, name="buy_product"),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
     path("contact/", views.contact, name="contact"),
 ]
