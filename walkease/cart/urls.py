75a97d13af818cc249946be18966c95fdc553391
Farah-94 <farahshigri94@gmail.com>
2025-06-16 00:22:19 +0100

    cart

--- walkease/cart/urls.py
+++ walkease/cart/urls.py
@@ -5,7 +5,8 @@ from . import views  # This imports from walkease/cart/views.py
 app_name = "cart"
 
 urlpatterns = [  
-    path("view/", views.cart_view, name="view_cart"), 
+    path("view/", views.cart_view, name="view_cart"),
+    path("add/", views.add_to_cart, name="add_to_cart"),  # New URL pattern for add_to_cart
     path("signup/", views.signup, name="signup"),
     path("signin/", views.signin, name="signin"),  
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
