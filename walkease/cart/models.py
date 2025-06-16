d3dd4d41fc16f2b80f361ddd9e50db961951781a
Farah-94 <farahshigri94@gmail.com>
2025-06-16 01:16:10 +0100

    store

--- walkease/cart/models.py
+++ walkease/cart/models.py
@@ -3,14 +3,14 @@ from django.conf import settings
 from walkease.store.models import Product
 
 class CartItem(models.Model):
-    # existing fields
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
-
-    # Add the size field (customize max_length as needed)
     size = models.CharField(max_length=10, default="M")
+
+    class Meta:
+        app_label = 'cart'  # Use the name of your app here
 
     def __str__(self):
         return f"{self.product.name} - {self.size} - {self.quantity}"
