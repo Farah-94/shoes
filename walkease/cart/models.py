a67bd31d5827e2a5eb8a2b30e66af3de0bd93058
Farah-94 <farahshigri94@gmail.com>
2025-06-16 13:06:07 +0100

    store

--- walkease/cart/models.py
+++ walkease/cart/models.py
@@ -7,10 +7,8 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
-    size = models.CharField(max_length=10, default="M")
-
-    class Meta:
-        app_label = 'cart'  # Use the name of your app here
+    # Remove the default value 'M'
+    size = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
         return f"{self.product.name} - {self.size} - {self.quantity}"
