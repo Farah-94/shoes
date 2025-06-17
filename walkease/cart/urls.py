884cd31498912b676eb49ab064d902692c2a757c
Farah-94 <farahshigri94@gmail.com>
2025-06-17 02:24:22 +0100

    cart

--- walkease/cart/urls.py
+++ walkease/cart/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views  # Ensure you have a views.py file
-from .views import signup
+from . import views  # This imports from walkease/cart/views.py
 
 app_name = "cart"
 
-urlpatterns = [  
-   path("view/", views.cart_view, name="view_cart"), 
-   path("signup/", views.signup, name="signup"), 
-   path("signin/", views.signin, name="signin"),  
-   path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+urlpatterns = [
+    path("view/", views.cart_view, name="view_cart"),
+    path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("signup/", views.signup, name="signup"),
+    path("signin/", views.signin, name="signin"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
