2b7478a7abd57972d3679b6ef8eb3459122a3d23
Farah-94 <farahshigri94@gmail.com>
2025-06-17 12:29:10 +0100

    store

--- walkease/store/urls.py
+++ walkease/store/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "store"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),              # Home page at /
+    path("index/", views.index, name="index_alias"),    # Also accessible via /index/
     path("products/", views.product_list, name="product_list"),
     path("products/<slug:slug>/", views.product_list, name="product_list_by_cat"),
     path("products/<int:product_id>/", views.product_detail, name="product_detail"),
