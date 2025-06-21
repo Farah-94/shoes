c7c6ece8975148d356050405449314070e190846
Farah-94 <farahshigri94@gmail.com>
2025-06-21 18:49:41 +0100

    store

--- walkease/store/models.py
+++ walkease/store/models.py
@@ -35,7 +35,7 @@ class Product(models.Model):
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     stock       = models.PositiveIntegerField(default=0)
-    image       = models.ImageField(blank=True, null=True, upload_to='products/')
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     
     def __str__(self):
         return self.name
