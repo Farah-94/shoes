b2aece635d364cde1ea4678deb7e66f6d24db7df
Farah-94 <farahshigri94@gmail.com>
2025-06-22 16:49:16 +0100

    store app

--- walkease/store/admin.py
+++ walkease/store/admin.py
@@ -1,85 +1,93 @@
 import os
 from django.contrib import admin
 from django import forms
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+from django.templatetags.static import static
 from django.conf import settings
 
 from .models import Product, Review
 
 
 # ------------------------------------------------------------------------------
-# 1) Custom ModelForm so we can include readonly image preview
+# 1) Custom ModelForm: dropdown of filenames from your static gallery folder
 # ------------------------------------------------------------------------------
-# store/admin.py
 class ProductAdminForm(forms.ModelForm):
+    image = forms.ChoiceField(
+        choices=[],
+        required=False,
+        label="Image filename from store/gallery"
+    )
+
     class Meta:
         model = Product
         fields = "__all__"
-    image = forms.ChoiceField(choices=[
-        (f, f) for f in os.listdir(
-            os.path.join(settings.BASE_DIR, 'walkease/store/static/store/gallery')
-        )
-        if not f.startswith('.')
-    ])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        gallery_dir = os.path.join(
+            settings.BASE_DIR,
+            "walkease", "store", "static", "store", "gallery"
+        )
+        files = []
+        if os.path.isdir(gallery_dir):
+            for fname in sorted(os.listdir(gallery_dir)):
+                path = os.path.join(gallery_dir, fname)
+                if os.path.isfile(path) and not fname.startswith("."):
+                    files.append((fname, fname))
+
+        # prepend blank choice
+        self.fields["image"].choices = [("", "---------")] + files
 
 
 # ------------------------------------------------------------------------------
-# 2) ProductAdmin: upload image, show thumb & preview
+# 2) ProductAdmin: show thumbnails, preview & use dropdown field
 # ------------------------------------------------------------------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     form = ProductAdminForm
 
-    list_display    = ("name", "category", "price", "stock", "display_thumbnail")
-    list_editable   = ("price", "stock")
-    search_fields   = ("name", "description")
-    list_filter     = ("category", "price", "stock")
+    list_display  = ("name", "category", "price", "stock", "thumbnail")
+    list_editable = ("price", "stock")
+    search_fields = ("name", "description")
+    list_filter   = ("category", "price", "stock")
 
     readonly_fields = ("image_preview",)
     fieldsets = [
-        (
-            "Basic Info",
-            {
-                "fields": (
-                    "name",
-                    "category",
-                    "price",
-                    "stock",
-                    "description",
-                )
-            },
-        ),
-        (
-            "Product Image",
-            {
-                "fields": ("image", "image_preview"),
-                "description": "Upload the main product image here.",
-            },
-        ),
+        ("Basic Info", {
+            "fields": ("name", "category", "price", "stock", "description")
+        }),
+        ("Product Image", {
+            "fields": ("image", "image_preview"),
+            "description": "Pick a filename from store/static/store/gallery/",
+        }),
     ]
 
-    def display_thumbnail(self, obj):
-        if obj.image and hasattr(obj.image, "url"):
-            return mark_safe(
-                f'<img src="{obj.image.url}" width="50" height="50" '
-                f'style="object-fit:contain; border:1px solid #ccc;" />'
+    def thumbnail(self, obj):
+        if obj.image:
+            url = static(f"store/gallery/{obj.image}")
+            return format_html(
+                '<img src="{}" width="50" height="50" '
+                'style="object-fit:contain; border:1px solid #ccc" />',
+                url
             )
-        return "No Image"
-    display_thumbnail.short_description = "Thumb"
+        return "(No image)"
+    thumbnail.short_description = "Thumb"
 
     def image_preview(self, obj):
-        if obj.image and hasattr(obj.image, "url"):
-            return mark_safe(
-                f'<img src="{obj.image.url}" style="max-height:200px; '
-                f'object-fit:contain; border:1px solid #ccc;" />'
+        if obj.image:
+            url = static(f"store/gallery/{obj.image}")
+            return format_html(
+                '<img src="{}" style="max-height:200px; '
+                'object-fit:contain; border:1px solid #ccc;" />',
+                url
             )
-        return "No Image"
+        return "(No image)"
     image_preview.short_description = "Preview"
 
 
 # ------------------------------------------------------------------------------
-# 3) ReviewAdmin: simple list display/filter/search
+# 3) ReviewAdmin: unchanged
 # ------------------------------------------------------------------------------
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
