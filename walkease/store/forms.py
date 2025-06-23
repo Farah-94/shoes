636dc420800bd29eb3fed6199e3b17e12663a13f
Farah-94 <farahshigri94@gmail.com>
2025-06-23 18:31:36 +0100

    store

--- walkease/store/forms.py
+++ walkease/store/forms.py
@@ -2,14 +2,20 @@ from django import forms
 from .models import Review
 
 class ReviewForm(forms.ModelForm):
+    display = forms.BooleanField(
+        required=False,
+        initial=True,
+        label="Show this review publicly"
+    )
+
     class Meta:
-        model  = Review
-        fields = ["rating", "comment"]
+        model = Review
+        fields = ["rating", "comment", "display"]
         widgets = {
-            "rating":  forms.RadioSelect,
-            "comment": forms.Textarea(attrs={"rows":4, "placeholder":"Write your review…"})
+            "rating": forms.RadioSelect,
+            "comment": forms.Textarea(attrs={"rows": 4, "placeholder": "Write your review…"})
         }
         labels = {
-            "rating":  "Your Rating",
+            "rating": "Your Rating",
             "comment": "Your Review"
         }
