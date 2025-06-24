a29dc3c5e4f4e57f5b44d58fe2693a9a66cf579a
Farah-94 <farahshigri94@gmail.com>
2025-06-24 07:57:31 +0100

    store

--- walkease/store/models.py
+++ walkease/store/models.py
@@ -5,27 +5,27 @@ from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=100, unique=True)
-#     slug = models.SlugField(max_length=120, unique=True, blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=120, unique=True, blank=True)
 
-#     def save(self, *args, **kwargs):
-#         if not self.slug:
-#             self.slug = slugify(self.name)
-#         super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
-    # category    = models.ForeignKey(
-    #                   Category,
-    #                   related_name="products",
-    #                   on_delete=models.PROTECT,
-    #                   null=True,
-    #                   blank=True,
-    #               )
+    category    = models.ForeignKey(
+                      Category,
+                      related_name="products",
+                      on_delete=models.PROTECT,
+                      null=True,
+                      blank=True,
+                  )
     name        = models.CharField(max_length=255)
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
